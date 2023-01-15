e17343f46adb83d22ddc5dbbd6980e54e4b64c7a
Hiroya-W <hyuyukun@gmail.com>
2023-01-15 16:10:29 +0000

    :+1: Fix props card story

--- components/bingo/Card.stories.tsx
+++ components/bingo/Card.stories.tsx
@@ -2,40 +2,102 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
 
 import Card from "./Card";
+import { CardProps } from "./Card";
 
 export default {
   title: "Bingo/Card",
   component: Card,
   argTypes: {
-    clubNames: { control: "object" },
-    backgroundColors: { control: "object" },
+    stamps: { control: "object" },
   },
 } as ComponentMeta<typeof Card>;
 
 const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
-const clubNames = () => {
-  const clubs = [];
-  for (let i = 0; i < 9; i++) {
-    clubs.push("Club " + i);
-  }
-  return clubs;
+const card: CardProps = {
+  stamps: [
+    {
+      clubName: "Club 1",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "Club 2",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "Club 3",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "Club 4",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "Club 5",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "Club 6",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "Club 7",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "Club 8",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "Club 9",
+      backgroundColor: "#00FF00",
+    },
+  ],
 };
 
-const backgroundColors = [
-  "#00FF00",
-  "#FF0000",
-  "#00FF00",
-  "#FF0000",
-  "#00FF00",
-  "#FF0000",
-  "#00FF00",
-  "#FF0000",
-  "#00FF00",
-];
-
-export const Primary = Template.bind({});
-Primary.args = {
-  clubNames: clubNames(),
-  backgroundColors: backgroundColors,
+const longClub: CardProps = {
+  stamps: [
+    {
+      clubName: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg",
+      backgroundColor: "#00FF00",
+    },
+    {
+      clubName: "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+      backgroundColor: "#FF0000",
+    },
+    {
+      clubName: "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+      backgroundColor: "#00FF00",
+    },
+  ],
 };
+
+export const Default = Template.bind({});
+Default.args = card;
+
+export const LongClubName = Template.bind({});
+LongClubName.args = longClub;
