dc0e678df45592dbdbed13c4b332b3a5788c89a7
Hiroya-W <hyuyukun@gmail.com>
2023-01-15 15:35:26 +0900

    :mute: new-cap off

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "prettier",
   ],
   rules: {
+    "new-cap": ["off"],
     "require-jsdoc": ["off"],
     "import/order": ["error", { alphabetize: { order: "asc" } }],
     "@next/next/no-img-element": ["off"],
