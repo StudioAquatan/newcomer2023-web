032408e4f5968e1510665031f823d29fa24ef660
Hiroya-W <hyuyukun@gmail.com>
2023-01-15 17:41:37 +0900

    :+1: Use emotion css style

--- components/bingo/Card.tsx
+++ components/bingo/Card.tsx
@@ -1,9 +1,18 @@
+import { css } from "@emotion/react";
 import Stamp from "./Stamp";
 
 type CardProps = {
   clubNames: Array<string>;
   backgroundColors: Array<string>;
 };
+
+const cardStyle = css`
+  display: grid;
+  grid-template-rows: 150px 150px 150px;
+  grid-template-columns: 150px 150px 150px;
+  align-content: center;
+  justify-content: center;
+`;
 
 export default function Card({ clubNames, backgroundColors }: CardProps) {
   const stamps = () => {
@@ -18,16 +27,7 @@ export default function Card({ clubNames, backgroundColors }: CardProps) {
 
   return (
     <div>
-      <style jsx>{`
-        #container {
-          display: grid;
-          grid-template-rows: 150px 150px 150px;
-          grid-template-columns: 150px 150px 150px;
-          justify-content: center;
-          align-content: center;
-        }
-      `}</style>
-      <div id="container">{stamps()}</div>
+      <div css={cardStyle}>{stamps()}</div>
     </div>
   );
 }
--- components/bingo/Stamp.tsx
+++ components/bingo/Stamp.tsx
@@ -1,40 +1,40 @@
+import { css } from "@emotion/react";
+
 type StampProps = {
   clubName: string;
   backgroundColor: string;
 };
 
+const stampStyle = (backgroundColor: string) => css`
+  position: absolute;
+  width: 120px;
+  height: 120px;
+  background: ${backgroundColor};
+`;
+
+const clubNameStyle = css`
+  position: absolute;
+  top: 5%;
+  right: 5%;
+  bottom: 5%;
+  left: 5%;
+  align-items: center;
+
+  /* font-family: "Noto Sans JP"; */
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 29px;
+  color: #fcfcfc;
+  text-align: center;
+  word-break: break-all;
+`;
+
 export default function Stamp({ clubName, backgroundColor }: StampProps) {
   return (
     <div>
-      <style jsx>{`
-        .stamp {
-          position: absolute;
-          width: 120px;
-          height: 120px;
-          background: ${backgroundColor};
-        }
-        .stamp-clubname {
-          /* 部活名 */
-          position: absolute;
-          left: 5%;
-          right: 5%;
-          top: 5%;
-          bottom: 5%;
-
-          font-family: 'Noto Sans JP';
-          font-style: normal;
-          font-weight: 500;
-          font-size: 20px;
-          line-height: 29px;
-          display: flex;
-          align-items: center;
-          text-align: center;
-
-          color: #FCFCFC;
-          word-break: break-all;
-      `}</style>
-      <div className="stamp">
-        <div className="stamp-clubname">{clubName}</div>
+      <div css={[stampStyle(backgroundColor)]}>
+        <div css={clubNameStyle}>{clubName}</div>
       </div>
     </div>
   );
