4f584318368d46f27894c66d963659d082e7d44f
Hiroya-W <hyuyukun@gmail.com>
2023-01-21 18:29:42 +0900

    :construction: Add ClubShowcase animation
    Storybook上だとアニメーションが上手く動いていない

---
+++ components/clubs/ClubShowcase.stories.tsx
@@ -0,0 +1,77 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+
+import ClubShowcase, { ClubShowcaseProps } from "./ClubShowcase";
+
+export default {
+  title: "Clubs/ClubShowcase",
+  component: ClubShowcase,
+  argTypes: {
+    cards: { control: "object" },
+  },
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof ClubShowcase>;
+
+const Template: ComponentStory<typeof ClubShowcase> = (args) => (
+  <ClubShowcase {...args} />
+);
+
+const studioaquatan = {
+  clubName: "StudioAquatan",
+  description: "Make Aquatan Great Again",
+};
+
+const tooLongName = {
+  clubName: "ToooooooooooLongClubName",
+  description: "TooooooooooooooooooooooooooooooooooooooooooooLongDescription",
+};
+
+const cards: ClubShowcaseProps = {
+  cards: [
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+    studioaquatan,
+  ],
+};
+
+const longNameClubs: ClubShowcaseProps = {
+  cards: [
+    tooLongName,
+    tooLongName,
+    tooLongName,
+    tooLongName,
+    tooLongName,
+    tooLongName,
+    tooLongName,
+    tooLongName,
+  ],
+};
+
+const mixed: ClubShowcaseProps = {
+  cards: [
+    tooLongName,
+    studioaquatan,
+    tooLongName,
+    studioaquatan,
+    tooLongName,
+    studioaquatan,
+    tooLongName,
+    studioaquatan,
+  ],
+};
+
+export const Default = Template.bind({});
+Default.args = cards;
+
+export const LongClubName = Template.bind({});
+LongClubName.args = longNameClubs;
+
+export const Mixed = Template.bind({});
+Mixed.args = mixed;
