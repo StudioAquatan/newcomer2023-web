5f8116ce921a4e1ac666c382e9099e4e87ee1010
Hiroya-W <hyuyukun@gmail.com>
2023-01-22 23:03:51 +0900

    :sparkles: Place organization component in Hero

--- components/toppage/Hero.tsx
+++ components/toppage/Hero.tsx
@@ -1,7 +1,10 @@
 import { css } from "@emotion/react";
 
+import Organizations from "./Organizations";
+
 const heroStyle = css`
   display: flex;
+  flex-flow: column;
   align-items: center;
   justify-content: center;
   height: 80vh;
@@ -26,6 +29,7 @@ const contentPStyle = css`
 export default function Hero() {
   return (
     <div css={heroStyle}>
+      <Organizations />
       <div css={heroContentStyle}>
         <p css={contentPStyle}>部活動相性診断</p>
         <p css={contentPStyle}>×</p>
