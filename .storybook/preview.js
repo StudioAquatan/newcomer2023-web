0bee27c51c84e4cd3895c531ed14df69a34c32d9
Hiroya-W <hyuyukun@gmail.com>
2023-01-26 21:59:49 +0900

    :+1: Apply global css to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
-// import "../styles/globals.css";
+import { globalStyles } from "../styles/globals";
+import { Global } from "@emotion/react";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={globalStyles} />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
