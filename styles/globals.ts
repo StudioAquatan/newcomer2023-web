e290fd6ff6d672914310165c9ff84170253e0dc8
Hiroya-W <hyuyukun@gmail.com>
2023-01-26 21:59:28 +0900

    :art: Set font-size 62.5%

--- styles/globals.ts
+++ styles/globals.ts
@@ -7,6 +7,7 @@ export const globalStyles = css`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    font-size: 62.5%;
     background-color: #f7fff5;
   }
 `;
