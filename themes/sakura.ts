c63be08e63379da44570b33b622194653e770351
Hiroya-W <hyuyukun@gmail.com>
2023-01-28 21:00:28 +0900

    :sparkles: Defined sakura theme

---
+++ themes/sakura.ts
@@ -0,0 +1,20 @@
+export const sakura = {
+  colors: {
+    backgroundColor: "#F7FFF5",
+    normalTextColor: "#000000",
+    subTextColor: "#797979",
+    button: {
+      backgroundColor: "#FFC8DF",
+      normalTextColor: "#000000",
+      activeTextColor: "#512C3C",
+    },
+    clubCard: {
+      backgroundColor: "#FFFFFF",
+      titleTextColor: "#000000",
+      borderColor: "#8B8B8B",
+    },
+    stamp: {
+      normalTextColor: "#F6F6F6",
+    },
+  },
+};
