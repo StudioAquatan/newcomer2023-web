2025588ee578a5c2dc6720210721d125fbb2ea66
Hiroya-W <hyuyukun@gmail.com>
2023-01-30 01:09:15 +0900

    :+1: Set isMobile value to jotai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import { Global } from "@emotion/react";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import { getSelectorsByUserAgent } from "react-device-detect";
+// import { NextPageContext } from "next/types";
 import Layout from "../components/Layout";
+import { useSetIsMobile } from "../store/userAgent";
 import { globalStyles } from "../styles/globals";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { setIsMobile } = useSetIsMobile();
+
+  useEffect(() => {
+    const { isMobile } = getSelectorsByUserAgent(navigator.userAgent);
+    setIsMobile({ isMobile: isMobile });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <Layout>
       <Global styles={globalStyles} />
