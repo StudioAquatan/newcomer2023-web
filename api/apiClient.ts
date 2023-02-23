f38c892956e278fb74e65ea0b4cbab8f70e0789c
Hiroya-W <hyuyukun@gmail.com>
2023-02-24 01:49:07 +0900

    :+1: Type support by aspdia api client

---
+++ api/apiClient.ts
@@ -0,0 +1,11 @@
+import aspida from "@aspida/fetch";
+import { stringify } from "qs";
+import api from "../api/$api";
+
+export const apiClient = api(
+  aspida((...args) => fetch(...args), {
+    baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4010",
+    throwHttpErrors: true,
+    paramsSerializer: (s) => stringify(s, { arrayFormat: "brackets" }),
+  })
+);
