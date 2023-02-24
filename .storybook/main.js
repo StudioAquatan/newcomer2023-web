e6da7372a133ab9f4565e81c22af071ddf0b76e8
f0reachARR <f0reach@f0reach.me>
2023-02-24 14:45:25 +0900

    :wrench: #51 Activate SWC on Next.js
    While keeping Storybook working

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,14 +13,15 @@ module.exports = {
   // https://qiita.com/1994spagetian/items/579718b7091ac7e5ec8b
   babel: async (options) => {
     // emotion用の注入設定
-    // preset-react ローダーを取得してくる
-    const presetReact = options.presets.find((p) => /preset-react/.test(p[0]));
-    // preset-react　ローダのオプションを設定
-    presetReact[1] = {
-      ...presetReact[1],
-      runtime: "automatic",
-      importSource: "@emotion/react",
-    };
+    options.presets.push([
+      "next/babel",
+      {
+        "preset-react": {
+          runtime: "automatic",
+          importSource: "@emotion/react",
+        },
+      },
+    ]);
     // Emotionプラグインを追加
     options.plugins.push(require.resolve("@emotion/babel-plugin"));
 
