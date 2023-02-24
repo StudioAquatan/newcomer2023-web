0bb6d75fcb3d574409e8bd7e5de289ae45e2875d
Hiroya-W <hyuyukun@gmail.com>
2023-02-24 23:18:26 +0900

    :+1: Add questions mock api

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from "msw";
 import { mockOrgsPostSuccess, mockOrgsPostInvalid } from "./api/orgs";
+import { mockQuestionsGetSuccess } from "./api/questions";
 import { mockUserPostInvalid, mockUserPostSuccess } from "./api/user";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4041";
@@ -12,6 +13,9 @@ export const handlers = {
   orgsGet: {
     success: rest.get(API_URL + "/orgs", mockOrgsPostSuccess),
     invalid: rest.get(API_URL + "/orgs", mockOrgsPostInvalid),
+  },
+  questionsGet: {
+    success: rest.get(API_URL + "/questions", mockQuestionsGetSuccess),
   },
 };
 
