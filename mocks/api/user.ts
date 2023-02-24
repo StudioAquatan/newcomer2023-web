aca46ea2d8e9fb852d756082086e4f39075ef614
Hiroya-W <hyuyukun@gmail.com>
2023-02-24 16:10:57 +0900

    :+1: Fix missing token user api response

--- mocks/api/user.ts
+++ mocks/api/user.ts
@@ -7,9 +7,12 @@ export const mockUserPostSuccess: ResponseResolver<
   return res(
     ctx.status(200),
     ctx.json({
-      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      nickname: "Test User",
-      createdAt: "2021-01-01T00:00:00.000Z",
+      token: "1181a584-28a1-8c32-4d75-4e1b6613bca8",
+      user: {
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        nickname: "Test User",
+        createdAt: "2021-01-01T00:00:00.000Z",
+      },
     })
   );
 };
