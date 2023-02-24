2f270cbc5efacb98bc33b24305a9b869602c5a36
Hiroya-W <hyuyukun@gmail.com>
2023-02-24 23:18:49 +0900

    :sparkles: Place progress bar

---
+++ pages/diagnose.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Question } from "../api/@types";
+import { apiClient } from "../api/apiClient";
+import ProgressBar from "../components/questions/ProgressBar";
+
+type DiagnoseProps = {
+  questions: Array<Question>;
+  initialCurrent: number;
+};
+
+export default function Diagnose({ questions, initialCurrent }: DiagnoseProps) {
+  const [current] = useState(initialCurrent);
+  return (
+    <div>
+      <ProgressBar questions={questions} current={current} />
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const questions = await apiClient.questions
+    .$get()
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      console.log(error);
+      return [
+        {
+          id: "999",
+          questionText: "Fetch Error: Question Not Found",
+          questionType: "five",
+        },
+      ];
+    });
+
+  return {
+    props: {
+      questions: questions,
+      initialCurrent: 0,
+    },
+  };
+}
