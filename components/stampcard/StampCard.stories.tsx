bbe9cdd2a6f5a9a572822d2fd1a4baca98250f95
Hiroya-W <hyuyukun@gmail.com>
2023-02-26 01:05:43 +0900

    :+1: Set seed for stamp

--- components/stampcard/StampCard.stories.tsx
+++ components/stampcard/StampCard.stories.tsx
@@ -18,91 +18,107 @@ export default {
 const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
 const card: StampCardProps = {
-  userId: 0,
   stamps: [
     {
       orgName: "Org 1",
       backgroundColor: "#00FF00",
+      seed: 1,
     },
     {
       orgName: "Org 2",
       backgroundColor: "#FF0000",
+      seed: 2,
     },
     {
       orgName: "Org 3",
       backgroundColor: "#00FF00",
+      seed: 3,
     },
     {
       orgName: "Org 4",
       backgroundColor: "#FF0000",
+      seed: 4,
     },
     {
       orgName: "Org 5",
       backgroundColor: "#00FF00",
+      seed: 5,
     },
     {
       orgName: "Org 6",
       backgroundColor: "#FF0000",
+      seed: 6,
     },
     {
       orgName: "Org 7",
       backgroundColor: "#00FF00",
       visited: true,
+      seed: 7,
     },
     {
       orgName: "Org 8",
       backgroundColor: "#FF0000",
       visited: true,
+      seed: 8,
     },
     {
       orgName: "Org 9",
       backgroundColor: "#00FF00",
       visited: true,
+      seed: 9,
     },
   ],
 };
 
 const longOrg: StampCardProps = {
-  userId: 0,
   stamps: [
     {
       orgName: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
       backgroundColor: "#00FF00",
+      seed: 1,
     },
     {
       orgName: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
       backgroundColor: "#FF0000",
+      seed: 2,
     },
     {
       orgName: "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc",
       backgroundColor: "#00FF00",
+      seed: 3,
     },
     {
       orgName: "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
       backgroundColor: "#FF0000",
+      seed: 4,
     },
     {
       orgName: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
       backgroundColor: "#00FF00",
+      seed: 5,
     },
     {
       orgName: "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
       backgroundColor: "#FF0000",
+      seed: 6,
     },
     {
       orgName: "gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg",
       backgroundColor: "#00FF00",
       visited: true,
+      seed: 7,
     },
     {
       orgName: "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
       backgroundColor: "#FF0000",
       visited: true,
+      seed: 8,
     },
     {
       orgName: "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
       backgroundColor: "#00FF00",
       visited: true,
+      seed: 9,
     },
   ],
 };
