98cd579c374f7fb375140d2345d0d451e67aac96
Hiroya-W <hyuyukun@gmail.com>
2023-02-26 22:04:24 +0900

    :+1: Ingertated organizations/organizationsMap

--- hooks/organizations.ts
+++ hooks/organizations.ts
@@ -2,36 +2,21 @@ import useSWR from "swr";
 import { apiClient } from "../api/apiClient";
 
 export function useOrganizations() {
-  return useSWR(
-    "/orgs",
-    async () => {
-      const res = await apiClient.orgs
-        .$get()
-        .then((res) => {
-          return res;
-        })
-        .catch((err) => {
-          throw new Error("Failed to fetch /orgs.", err);
-        });
+  return useSWR("/orgs", async () => {
+    const organizations = await apiClient.orgs
+      .$get()
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        throw new Error("Failed to fetch /orgs.", err);
+      });
 
-      return res;
-    },
-    { revalidateOnFocus: false }
-  );
-}
+    const orgMap = new Map(organizations.map((org) => [org.id, org]));
 
-export function useOrganizationsMap() {
-  const { data: organizations } = useOrganizations();
-  return useSWR(
-    "/orgsMap",
-    () => {
-      if (!organizations) {
-        throw new Error("Empty organizations. Failed to fetch /orgsMap.");
-      }
-
-      const orgMap = new Map(organizations.map((org) => [org.id, org]));
-      return orgMap;
-    },
-    { revalidateOnFocus: false }
-  );
+    return {
+      organizations: organizations,
+      organizationsMap: orgMap,
+    };
+  });
 }
