11e2987713cb01620c185c8b3a14f4b2cb35ad63
Hiroya-W <hyuyukun@gmail.com>
2023-02-27 01:19:43 +0900

    :sparkles: One Question Component

---
+++ components/questions/OneQuestion.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+import { Question as QuestionType } from "../../api/@types";
+
+import OneQuestion from "./OneQuestion";
+
+export default {
+  title: "Questions/OneQuestion",
+  component: OneQuestion,
+} as ComponentMeta<typeof OneQuestion>;
+
+const Template: ComponentStory<typeof OneQuestion> = (args) => (
+  <OneQuestion {...args} />
+);
+
+const question: QuestionType = {
+  id: "0",
+  questionText: "質問文",
+  questionType: "five",
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  question: question,
+};
---
+++ components/questions/OneQuestion.tsx
@@ -0,0 +1,23 @@
+import { css } from "@emotion/react";
+import { Question as QuestionType } from "../../api/@types";
+import FiveRadioButton from "../buttons/FiveRadioButton";
+
+type QuestionProps = {
+  question: QuestionType;
+};
+
+const container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// TODO: ここで、question.questionTypeに応じて、適切なコンポーネントを呼び出す
+export default function OneQuestion({ question }: QuestionProps) {
+  return (
+    <div css={container}>
+      <div>Q. {question.questionText}</div>
+      <FiveRadioButton questionId={question.id} />
+    </div>
+  );
+}
