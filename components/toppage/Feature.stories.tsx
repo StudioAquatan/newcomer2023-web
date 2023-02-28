d9803f49a2c3262756fc0f3aab7f3ce0f17ac7d6
Hiroya-W <hyuyukun@gmail.com>
2023-02-28 23:16:08 +0900

    :+1: Changed props received ReactNode

--- components/toppage/Feature.stories.tsx
+++ components/toppage/Feature.stories.tsx
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
 
@@ -15,18 +16,34 @@ const Template: ComponentStory<typeof Feature> = (args) => (
   <Feature {...args} />
 );
 
+const featureContentStyle = css`
+  width: 100%;
+`;
+
+const diagnoseContent = (
+  <img src="/toppage/diagnose.png" alt="相性診断" css={featureContentStyle} />
+);
+
+const stampRallyContent = (
+  <img
+    src="/toppage/stamp-rally.png"
+    alt="スタンプラリー"
+    css={featureContentStyle}
+  />
+);
+
 export const Diagnose = Template.bind({});
 Diagnose.args = {
   title: "相性診断",
   description:
     "BINGOスタンプラリーのために相性診断をして自分に合った部・サークルの説明を聞きに行こう！",
-  featureImagePath: "/toppage/diagnose.png",
+  featureContentNode: diagnoseContent,
 };
 
 export const StampRally = Template.bind({});
 StampRally.args = {
   title: "スタンプラリー",
   description: "QRコードを読み込んで、景品を貰いに行こう！",
-  featureImagePath: "/toppage/stamp-rally.png",
+  featureContentNode: stampRallyContent,
   inverse: true,
 };
