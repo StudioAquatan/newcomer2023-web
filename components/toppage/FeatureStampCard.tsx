d1c1517ba844363c5a2f55392fb2c7bfd8b23d3d
Hiroya-W <hyuyukun@gmail.com>
2023-03-01 00:22:36 +0900

    :sparkles: Add FeatureStampCard component

---
+++ components/toppage/FeatureStampCard.tsx
@@ -0,0 +1,46 @@
+import { Recommendation } from "../../api/@types";
+import { StampProps } from "../stampcard/Stamp";
+import StampCard, { StampCardProps } from "../stampcard/StampCard";
+import Feature from "./Feature";
+
+type FeatureStampCardProps = {
+  title: string;
+  description: string;
+  inverse?: boolean;
+  recommendation: Recommendation;
+};
+
+export default function FeatureStampCard({
+  title,
+  description,
+  inverse = false,
+  recommendation,
+}: FeatureStampCardProps) {
+  const seed = 0;
+
+  // TODO: 全団体からランダムに9団体選ぶようにする
+  const stamps: StampProps[] = recommendation.orgs.map(
+    (recommendationItem, index) => {
+      return {
+        recommendation: recommendationItem,
+        seed: seed + index,
+      };
+    }
+  );
+
+  const stampCardProps: StampCardProps = {
+    stamps: stamps,
+    size: { maxWidth: "48rem", maxHeight: "80vw" },
+  };
+
+  const featureContentNode = <StampCard {...stampCardProps} />;
+
+  const featureProps = {
+    title: title,
+    description: description,
+    featureContentNode: featureContentNode,
+    inverse: inverse,
+  };
+
+  return <Feature {...featureProps} />;
+}
