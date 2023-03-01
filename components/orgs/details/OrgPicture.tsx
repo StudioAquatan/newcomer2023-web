6189d0a17d193c9d51d85fd0082be5bbd6287e3a
f0reachARR <f0reach@f0reach.me>
2023-03-01 11:17:17 +0900

    :sparkles: Add hls.js based video player for org details

--- components/orgs/details/OrgPicture.tsx
+++ components/orgs/details/OrgPicture.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import Image from "next/image";
+import React from "react";
 
 type Props = {
   url: string;
@@ -18,16 +19,32 @@ const container = css`
 
 const image = css`
   width: auto;
+  min-width: calc(min(100vw, 400px));
   max-width: 100vw;
   height: auto;
   max-height: 90vh;
 `;
 
 export default function OrgPicture({ url, isMovie, width, height }: Props) {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+  React.useEffect(() => {
+    if (!isMovie || !videoRef.current) return;
+    if (!videoRef.current.canPlayType("application/vnd.apple.mpegURL")) {
+      const startWithHls = async () => {
+        const Hls = await import("hls.js");
+        const player = new Hls.default();
+        player.startLevel = -1;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        player.attachMedia(videoRef.current!);
+        player.loadSource(url);
+      };
+      startWithHls();
+    }
+  }, [isMovie, url]);
   return (
     <div css={container}>
       {isMovie ? (
-        <video src={url} css={image} autoPlay />
+        <video src={url} css={image} autoPlay muted ref={videoRef} />
       ) : (
         <Image
           src={url}
