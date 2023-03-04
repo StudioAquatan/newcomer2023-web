8598f5c02197fe9a09df5f22d2a0673b16c56c49
Shota Minami <shouth373@gmail.com>
2023-03-05 01:38:31 +0900

    :clown_face: Update mock implementation

--- mocks/index.ts
+++ mocks/index.ts
@@ -2,12 +2,15 @@ import { setupWorker } from "msw";
 import { setupServer } from "msw/node";
 import { defaultHandlers } from "./handlers";
 
-export const initMocks = () => {
-  if (typeof window == "undefined") {
+export const initMockServer = () => {
+  if (typeof window === "undefined") {
     setupServer(...defaultHandlers).listen();
     console.log("[MSW] Init Mock Server");
-  } else {
-    void setupWorker(...defaultHandlers).start();
-    console.log("[MSW] Init Mock Worker");
   }
-}
+};
+
+export const initMockWorker = () => {
+  if (typeof window !== "undefined") {
+    return setupWorker(...defaultHandlers).start();
+  }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getSelectorsByUserAgent } from "react-device-detect";
 // import { NextPageContext } from "next/types";
 import Layout from "../components/Layout";
-import { initMocks } from "../mocks";
+import { initMockServer, initMockWorker } from "../mocks";
 import { useSetIsMobile } from "../store/userAgent";
 import { globalStyles } from "../styles/globals";
 import { sakura } from "../themes/sakura";
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-  initMocks();
+const isMocking = process.env.NEXT_PUBLIC_API_MOCKING === "enabled";
+
+if (isMocking) {
+  initMockServer();
 }
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -22,6 +24,22 @@ export default function App({ Component, pageProps }: AppProps) {
     const { isMobile } = getSelectorsByUserAgent(navigator.userAgent);
     setIsMobile({ isMobile: isMobile });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // モック用のService Workerが登録中かどうかのフラグ
+  const [ isMockWorkerRegistering, setIsMockWorkerRegistering ] = useState(isMocking);
+  useEffect(() => {
+    // フラグが登録中を示す(=== true)なら実際に登録作業を行ってフラグをfalseにする
+    if (isMockWorkerRegistering) {
+      initMockWorker()?.then(() => void setIsMockWorkerRegistering(false));
+    }
+  });
+
+  // フラグが登録中を示す(=== true)なら登録中の旨を表示する
+  if (isMockWorkerRegistering) {
+    return (
+      <div>Mock worker has not registered yet.</div>
+    );
+  }
 
   return (
     <ThemeProvider theme={sakura}>
