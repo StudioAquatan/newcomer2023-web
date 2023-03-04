9eba74f2af678b36084761917424067c885c36f4
Hiroya-W <hyuyukun@gmail.com>
2023-03-04 11:23:35 +0900

    :bug: Fix missing fonts

--- styles/fonts/GenShinGoithcP.ts
+++ styles/fonts/GenShinGoithcP.ts
@@ -12,33 +12,35 @@ const GenShinGothicP = css`
     font-family: GenShinGothic-P;
     font-style: normal;
     font-weight: 300;
-    src: url("fonts/GenShinGothicP/GenShinGothic-P-Light.woff2") format("woff2");
+    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Light.woff2")
+      format("woff2");
   }
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
     font-weight: 400;
-    src: url("fonts/GenShinGothicP/GenShinGothic-P-Normal.woff2")
+    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Normal.woff2")
       format("woff2");
   }
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
     font-weight: 500;
-    src: url("fonts/GenShinGothicP/GenShinGothic-P-Medium.woff2")
+    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Medium.woff2")
       format("woff2");
   }
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
     font-weight: 700;
-    src: url("fonts/GenShinGothicP/GenShinGothic-P-Bold.woff2") format("woff2");
+    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Bold.woff2") format("woff2");
   }
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
     font-weight: 900;
-    src: url("fonts/GenShinGothic-P-Heavy.woff2") format("woff2");
+    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Heavy.woff2")
+      format("woff2");
   }
 `;
 
