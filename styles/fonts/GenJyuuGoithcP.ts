9e8d73bb0237e560e5c2c3d27b74fdcecab536fd
Hiroya-W <hyuyukun@gmail.com>
2023-03-04 11:30:25 +0900

    :sparkles: Add GenJyuuGothicP font

---
+++ styles/fonts/GenJyuuGoithcP.ts
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+
+const GenJyuuGothicP = css`
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 200;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-ExtraLight.woff2")
+      format("woff2");
+  }
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 300;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-Light.woff2")
+      format("woff2");
+  }
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 400;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-Normal.woff2")
+      format("woff2");
+  }
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 500;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-Medium.woff2")
+      format("woff2");
+  }
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 700;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-Bold.woff2") format("woff2");
+  }
+  @font-face {
+    font-family: GenJyuuGothic-P;
+    font-style: normal;
+    font-weight: 900;
+    src: url("/fonts/GenJyuuGothicP/GenJyuuGothic-P-Heavy.woff2")
+      format("woff2");
+  }
+`;
+
+export { GenJyuuGothicP };
