1946f53fab3420458e9e97adb322f98c7ef0631a
f0reachARR <f0reach@f0reach.me>
2023-03-06 21:53:10 +0900

    :bug: Non-awaiting promise

--- api-client/cached-response.ts
+++ api-client/cached-response.ts
@@ -1,15 +1,19 @@
+import { OrganizationFull } from "./@types";
 import { apiClient } from "./apiClient";
 
 // getStaticPropsで使うレスポンス群を保持するための部分
-const orgsPromise = apiClient.orgs
-  .$get()
-  .then((res) => {
-    return res;
-  })
-  .catch((error) => {
-    throw Error(error);
-  });
+let orgsPromise: Promise<OrganizationFull[]> | null = null;
 
 export function getOrgs() {
+  orgsPromise =
+    orgsPromise ??
+    apiClient.orgs
+      .$get()
+      .then((res) => {
+        return res;
+      })
+      .catch((error) => {
+        throw Error(error);
+      });
   return orgsPromise;
 }
