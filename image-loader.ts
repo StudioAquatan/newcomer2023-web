5e185916f8e99b9e86f1e2590bfb9a3fdcddb66a
f0reachARR <f0reach@f0reach.me>
2023-03-07 00:57:40 +0900

    :zap: Use imgix for assets

--- image-loader.ts
+++ image-loader.ts
@@ -1,10 +1,11 @@
 import { ImageLoaderProps } from "next/image";
 export default function imgixLoader({ src, width, quality }: ImageLoaderProps) {
   const newtPrefix = process.env.NEXT_PUBLIC_NEWT_ROOT ?? "newt/";
+  const assetsPrefix = process.env.NEXT_PUBLIC_RESOURCE_URL ?? "assets/";
   if (src.startsWith(newtPrefix)) {
     src = src.replace(newtPrefix, "newt/");
-  } else if (src.startsWith("https://resource.local")) {
-    src = src.replace("https://resource.local/", "assets/");
+  } else if (src.startsWith(assetsPrefix)) {
+    src = src.replace(assetsPrefix, "assets");
   } else {
     return src;
   }
