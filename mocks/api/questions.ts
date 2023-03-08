2c4fdba9baf8b9a66f9a718dbac9fea6430d5e5d
Hiroya-W <hyuyukun@gmail.com>
2023-03-08 22:50:09 +0900

    :+1: Response YesNo,Choise Questions

--- mocks/api/questions.ts
+++ mocks/api/questions.ts
@@ -1,20 +1,69 @@
 import { MockedRequest, ResponseResolver, restContext } from "msw";
+import { Question } from "../../api-client/@types";
 
-const questions = [
+export const questionsGetSuccessResponseJson: Question[] = [
   {
     id: "1",
-    questionText: "Question 1",
-    questionType: "five",
+    questionText: "あなたの課程は？",
+    questionType: "choice",
+    answers: [
+      {
+        id: 1,
+        text: "応用生物学課程",
+      },
+      {
+        id: 2,
+        text: "応用化学課程",
+      },
+      {
+        id: 3,
+        text: "生体分子応用化学課程",
+      },
+      {
+        id: 4,
+        text: "高分子機能工学課程",
+      },
+      {
+        id: 5,
+        text: "物質工学課程",
+      },
+      {
+        id: 6,
+        text: "電子システム工学課程",
+      },
+      {
+        id: 7,
+        text: "情報工学課程",
+      },
+      {
+        id: 8,
+        text: "機械工学課程",
+      },
+      {
+        id: 9,
+        text: "デザイン・建築学課程",
+      },
+    ],
   },
   {
     id: "2",
-    questionText: "Question 2",
-    questionType: "five",
+    questionText: "あなたはデザイン・建築学課程ですか？",
+    questionType: "yesno",
   },
   {
     id: "3",
-    questionText: "Question 3",
-    questionType: "five",
+    questionText: "あなたの性別は？",
+    questionType: "choice",
+    answers: [
+      {
+        id: 1,
+        text: "男性",
+      },
+      {
+        id: 2,
+        text: "女性",
+      },
+    ],
   },
   {
     id: "4",
@@ -47,5 +96,5 @@ export const mockQuestionsGetSuccess: ResponseResolver<
   MockedRequest,
   typeof restContext
 > = async (req, res, ctx) => {
-  return res(ctx.status(200), ctx.json(questions));
+  return res(ctx.status(200), ctx.json(questionsGetSuccessResponseJson));
 };
