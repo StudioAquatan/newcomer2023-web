04be98050b12a55ac2a31b1a44bd5808270917d4
Hiroya-W <hyuyukun@gmail.com>
2023-03-10 23:46:43 +0900

    :sparkles: Put QuestionResult to API server

--- components/questions/QuestionForm.tsx
+++ components/questions/QuestionForm.tsx
@@ -1,19 +1,54 @@
+import { useRouter } from "next/router";
 import {
   Question as QuestionType,
   QuestionResult,
 } from "../../api-client/@types";
+import { apiClient } from "../../api-client/apiClient";
+import useUser from "../../hooks/user";
 import OneQuestion from "./OneQuestion";
 
 type QuestionFormProps = {
   questions: QuestionType[];
 };
 
+const putRecommendation = async (token: string, answers: QuestionResult[]) => {
+  return await apiClient.recommendation.put({
+    body: answers,
+    config: {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  });
+};
+
 export default function QuestionForm({ questions }: QuestionFormProps) {
+  const router = useRouter();
+  const { data: userData } = useUser();
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
   const answers = new Map<string, QuestionResult>();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(answers);
+    const res = await putRecommendation(
+      userData.token,
+      Array.from(answers.values())
+    );
+    if (res.status == 200) {
+      // 正しく診断を終えたらスタンプカードに遷移する
+      router.push("/stampcard");
+    }
+    // TODO: それ以外のときのエラーハンドリングをいい感じにする
+    else if (res.status == 400) {
+      throw new Error("質問が不足している場合など");
+    } else if (res.status == 401) {
+      throw new Error("Unauthorized");
+    } else if (res.status == 429) {
+      throw new Error("再診断の残り回数が0になった");
+    }
   };
 
   const onChange = (questionId: string, answerId: number) => {
