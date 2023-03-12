f604fddb2cf815152d0ff0cc3be31c3f74249f12
Hiroya-W <hyuyukun@gmail.com>
2023-03-12 19:46:45 +0900

    :recycle: Refactoring Header component

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,5 +1,51 @@
-import { css, Theme, useTheme } from "@emotion/react";
+import { css, Theme } from "@emotion/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type LinkData = {
+  href: string;
+  external?: boolean;
+};
+
+type HeaderData = {
+  label: string;
+  targetDevice?: "mobile" | "desktop";
+  link?: LinkData;
+};
+
+const headersData: HeaderData[] = [
+  {
+    label: "新歓について",
+    link: {
+      href: "",
+      external: true,
+    },
+  },
+  {
+    label: "団体一覧",
+    link: {
+      href: "/orgs",
+    },
+  },
+  {
+    label: "スタンプカード",
+    targetDevice: "mobile",
+    link: {
+      href: "/stampcard",
+    },
+  },
+  {
+    label: "診断を始める",
+    targetDevice: "mobile",
+    link: {
+      href: "/diagnose",
+    },
+  },
+  // {
+  //   label: "スマホからアクセスして診断を始めてみよう！",
+  //   targetDevice: "desktop",
+  // },
+];
 
 const container = (theme: Theme) => css`
   display: flex;
@@ -7,6 +53,34 @@ const container = (theme: Theme) => css`
   padding: 1rem 2rem;
   color: ${theme.colors.normalTextColor};
   background-color: transparent;
+`;
+
+const siteNameStyle = (theme: Theme) => css`
+  padding: 0;
+  margin: 0;
+  font-size: 2rem;
+
+  a {
+    color: ${theme.colors.normalTextColor};
+    text-decoration: none;
+  }
+`;
+
+const navMenuItemsStyle = (theme: Theme) => css`
+  margin: 0 0 0 auto;
+
+  ul {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  li {
+    display: inline;
+    margin: 0 0 0 3rem;
+    font-size: 1.6rem;
+  }
 
   a {
     color: ${theme.colors.normalTextColor};
@@ -19,68 +93,97 @@ const container = (theme: Theme) => css`
   }
 `;
 
-const siteNameStyle = css`
-  padding: 0;
-  margin: 0;
-  font-size: 2rem;
-`;
-
-const navItemsStyle = css`
-  margin: 0 0 0 auto;
-
-  & > ul {
-    display: flex;
-    padding: 0;
-    margin: 0;
-    list-style: none;
-  }
-
-  & > ul > li {
-    display: inline;
-    margin: 0 0 0 3rem;
-    font-size: 1.6rem;
-  }
-`;
-
 export type HeaderProps = {
   isMobile: boolean;
 };
 
 export default function Header({ isMobile }: HeaderProps) {
-  const theme = useTheme();
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+  // TODO: drawerOpen を使う
+  const { mobileView } = state;
 
-  return (
-    <header css={container(theme)}>
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 800
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  const SiteName = () => {
+    return (
       <h1 css={siteNameStyle}>
         <Link href="/">京都工芸繊維大学 新歓サイト</Link>
       </h1>
-      <nav css={navItemsStyle}>
+    );
+  };
+
+  const LinkItem = ({ label, targetDevice, link }: HeaderData) => {
+    if (targetDevice === "mobile" && !isMobile) {
+      return null;
+    }
+    if (targetDevice === "desktop" && isMobile) {
+      return null;
+    }
+
+    return (
+      <li key={label}>
+        {link ? (
+          link.external ? (
+            <a href={link.href} target="_blank" rel="noopener noreferrer">
+              {label}
+            </a>
+          ) : (
+            <Link href={link.href}>{label}</Link>
+          )
+        ) : (
+          // リンク先がない場合は、pタグでラベルを表示する
+          <p>{label}</p>
+        )}
+      </li>
+    );
+  };
+
+  const MenuButtons = () => {
+    return (
+      <nav css={navMenuItemsStyle}>
         <ul>
-          <li>
-            <a href="">新歓について</a>
-          </li>
-          <li>
-            <Link href="/orgs">団体一覧</Link>
-          </li>
-          {isMobile && (
-            <>
-              <li>
-                <Link href="/stampcard">スタンプカード</Link>
-              </li>
-              <li>
-                <Link href="/diagnose">診断を始める</Link>
-              </li>
-            </>
-          )}
-          {!isMobile && (
-            <>
-              <li>
-                <p>スマホからアクセスして診断を始めてみよう！</p>
-              </li>
-            </>
-          )}
+          {headersData.map((data, index) => {
+            return <LinkItem key={index} {...data} />;
+          })}
         </ul>
       </nav>
-    </header>
-  );
+    );
+  };
+
+  const displayDesktop = () => {
+    return (
+      <div css={container}>
+        <SiteName />
+        <MenuButtons />
+      </div>
+    );
+  };
+
+  const displayMobile = () => {
+    return (
+      <>
+        <SiteName />
+        <MenuButtons />
+      </>
+    );
+  };
+
+  return <header>{mobileView ? displayMobile() : displayDesktop()}</header>;
 }
