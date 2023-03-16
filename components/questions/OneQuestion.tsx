bc687db73fe92ed26e183db966a3501f7312e4d0
Hiroya-W <hyuyukun@gmail.com>
2023-03-16 23:10:43 +0900

    :art: Add spaces diagnose compornents

--- components/questions/OneQuestion.tsx
+++ components/questions/OneQuestion.tsx
@@ -14,6 +14,7 @@ type QuestionProps = {
 const container = (transition: boolean) => css`
   display: flex;
   flex-direction: column;
+  gap: 2rem;
   align-items: center;
   padding: 0;
   margin: 0;
