d2ed09622b4b04e9351ffb1b3468965720445474
f0reachARR <f0reach@f0reach.me>
2023-03-16 15:35:59 +0900

    :sparkles: Add limit reached message

--- components/questions/LimitNotice.tsx
+++ components/questions/LimitNotice.tsx
@@ -23,19 +23,26 @@ type Props = {
 export default function LimitNotice({ remain, back, close }: Props) {
   return (
     <>
-      <ModalTitle>再診断の確認</ModalTitle>
+      <ModalTitle>{remain > 0 ? "再診断の確認" : "おっと？"}</ModalTitle>
       <ModalContent>
-        <p css={textStyle}>
-          診断できる回数には制限があります． <wbr />
-          <span css={remainStyle}>残り{remain}回</span>
-          診断できます．
-          <wbr />
-        </p>
-        <p css={textStyle}>続けますか？</p>
+        {remain > 0 ? (
+          <>
+            <p css={textStyle}>
+              診断できる回数には制限があります． <wbr />
+              <span css={remainStyle}>残り{remain}回</span>
+              診断できます．
+            </p>
+            <p css={textStyle}>続けますか？</p>
+          </>
+        ) : (
+          <p css={textStyle}>
+            診断できる回数を越えたため、もう診断することができません
+          </p>
+        )}
       </ModalContent>
       <ModalButtonContainer>
         <ModalButton label="戻る" onClick={back} />
-        <ModalButton label="続ける" onClick={close} />
+        <ModalButton label="続ける" onClick={close} disabled={remain === 0} />
       </ModalButtonContainer>
     </>
   );
