3c0922d96f4b2043b1437e2e26ab2f6cf5f27acc
f0reachARR <f0reach@f0reach.me>
2023-03-16 12:59:06 +0900

    :sparkles: Add modal controller

---
+++ components/modal/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Modal from "./Modal";
+
+export function useModal() {
+  const [isOpen, setOpen] = React.useState(false);
+
+  const open = () => setOpen(true);
+  const close = () => setOpen(false);
+
+  const ModalWrapper = React.useCallback(
+    ({ children }: React.PropsWithChildren) => {
+      return isOpen ? <Modal>{children}</Modal> : null;
+    },
+    [isOpen]
+  );
+
+  return { open, close, ModalWrapper };
+}
