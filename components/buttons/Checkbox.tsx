d96cc9cdb0ab04abf61625d2fefa9e8da584d9db
f0reachARR <f0reach@f0reach.me>
2023-03-19 12:56:48 +0900

    :recycle: Improve checkbox CSS

--- components/buttons/Checkbox.tsx
+++ components/buttons/Checkbox.tsx
@@ -5,52 +5,66 @@ const container = css`
 
   input {
     display: none;
-  }
 
-  input + label {
-    position: relative;
-    display: inline;
-    padding-left: 1.5em;
-    cursor: pointer;
-    user-select: none;
+    + label {
+      position: relative;
+      display: inline;
+      padding-left: 1.5em;
+      cursor: pointer;
+      user-select: none;
 
-    &::before {
-      position: absolute;
-      top: calc(50% - (0.15em * 2 + 1em) / 2);
-      left: 0;
-      width: 1em;
-      height: 1em;
-      content: "";
-      border: 0.15em solid #aaa;
-      transition: border 0.1s ease-in-out;
+      &::before {
+        position: absolute;
+        top: calc(50% - (0.15em * 2 + 1em) / 2);
+        left: 0;
+        width: 1em;
+        height: 1em;
+        content: "";
+        border: 0.15em solid #aaa;
+        transition: border 0.1s ease-in-out;
+      }
+
+      &::after {
+        position: absolute;
+        top: calc(50% - (0.15em * 2 + 1em) / 2 + 0.2em);
+        left: 0.45em;
+        width: 0.3em;
+        height: 0.6em;
+        content: "";
+        border-right: 0.15em solid #ff8dbd;
+        border-bottom: 0.15em solid #ff8dbd;
+        opacity: 0;
+        transition: opacity 0.1s ease-in-out;
+        transform: rotate(45deg);
+      }
     }
 
-    &::after {
-      position: absolute;
-      top: calc(50% - (0.15em * 2 + 1em) / 2 + 0.2em);
-      left: 0.45em;
-      width: 0.3em;
-      height: 0.6em;
-      content: "";
-      border-right: 0.15em solid #ff8dbd;
-      border-bottom: 0.15em solid #ff8dbd;
-      opacity: 0;
-      transition: opacity 0.1s ease-in-out;
-      transform: rotate(45deg);
+    &:disabled {
+      + label::before {
+        background-color: #eee;
+        border-color: #aaa;
+      }
+
+      &:checked + label::after {
+        border-color: #aaa;
+        opacity: 1;
+      }
     }
 
-    &:hover::before {
-      border-color: #ffc8df;
-    }
-  }
+    &:not(:disabled) {
+      + label:hover::before {
+        border-color: #ffc8df;
+      }
 
-  input:checked + label {
-    &::before {
-      border-color: #ff8dbd;
-    }
+      &:checked + label {
+        &::before {
+          border-color: #ff8dbd;
+        }
 
-    &::after {
-      opacity: 1;
+        &::after {
+          opacity: 1;
+        }
+      }
     }
   }
 `;
@@ -60,6 +74,7 @@ type Props = {
   label: React.ReactNode;
   labelProps?: React.HTMLProps<HTMLLabelElement>;
   containerProps?: React.HTMLProps<HTMLDivElement>;
+  disabled?: boolean;
 } & Omit<React.HTMLProps<HTMLInputElement>, "type" | "id" | "label">;
 export default function Checkbox({
   id,
