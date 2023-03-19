6badb054ba09ec8e6823c926cb6fd338422696eb
f0reachARR <f0reach@f0reach.me>
2023-03-19 14:15:35 +0900

    :bento: Remove unused fonts

--- styles/fonts/GenShinGoithcP.ts
+++ styles/fonts/GenShinGoithcP.ts
@@ -4,22 +4,6 @@ const GenShinGothicP = css`
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
-    font-weight: 200;
-    src: url("/fonts/GenShinGothicP/GenShinGothic-P-ExtraLight.woff2")
-      format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: GenShinGothic-P;
-    font-style: normal;
-    font-weight: 300;
-    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Light.woff2")
-      format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: GenShinGothic-P;
-    font-style: normal;
     font-weight: 400;
     src: url("/fonts/GenShinGothicP/GenShinGothic-P-Normal.woff2")
       format("woff2");
@@ -28,24 +12,8 @@ const GenShinGothicP = css`
   @font-face {
     font-family: GenShinGothic-P;
     font-style: normal;
-    font-weight: 500;
-    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Medium.woff2")
-      format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: GenShinGothic-P;
-    font-style: normal;
     font-weight: 700;
     src: url("/fonts/GenShinGothicP/GenShinGothic-P-Bold.woff2") format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: GenShinGothic-P;
-    font-style: normal;
-    font-weight: 900;
-    src: url("/fonts/GenShinGothicP/GenShinGothic-P-Heavy.woff2")
-      format("woff2");
     font-display: swap;
   }
 `;
