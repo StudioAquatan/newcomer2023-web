153ba50a7fd614c9f21de4b84a8beb8ac0e298e2
Hiroya-W <hyuyukun@gmail.com>
2023-03-30 11:29:12 +0900

    :sparkles: Switch some fall animations

--- components/visited/Confetti.tsx
+++ components/visited/Confetti.tsx
@@ -10,7 +10,7 @@ const base = css`
   overflow: hidden;
 `;
 
-const fall = keyframes`
+const fallA = keyframes`
   0% {
     transform: translate(0%, 0);
   }
@@ -28,19 +28,57 @@ const fall = keyframes`
   }
 `;
 
+const fallB = keyframes`
+  0% {
+    transform: translate(75%, 0);
+  }
+  25% {
+    transform: translate(0, 25%);
+  }
+  50% {
+    transform: translate(-125%, 50%);
+  }
+  75% {
+    transform: translate(25%, 75%);
+  }
+  100% {
+    transform: translate(75%, 100%);
+  }
+`;
+
+const fallC = keyframes`
+  0% {
+    transform: translate(100%, 0);
+  }
+  25% {
+    transform: translate(-25%, 25%);
+  }
+  50% {
+    transform: translate(50%, 50%);
+  }
+  75% {
+    transform: translate(-75%, 75%);
+  }
+  100% {
+    transform: translate(100%, 100%);
+  }
+`;
+
 const piece = ({
   margin,
   delay,
   duration,
+  keyframe,
 }: {
   margin: string;
   delay: string;
   duration: string;
+  keyframe: Keyframes;
 }) => css`
   position: absolute;
   height: 100%;
   margin-left: ${margin};
-  animation: ${fall} linear infinite;
+  animation: ${keyframe} linear infinite;
   animation-duration: ${duration};
   animation-delay: ${delay};
 `;
@@ -58,17 +96,27 @@ export default function Confetti({ count, duration, delay }: ConfettiProps) {
   const pieces = React.useMemo(() => {
     const SEED = 0;
     const r = new Random(SEED);
+    const keyframes = [fallA, fallB, fallC];
+
     return (
       <>
         {[...Array(count)].map((_, i) => {
+          const selectedKeyframe =
+            keyframes[r.nextNumber(0, keyframes.length - 1)];
+
           return (
             <div
               key={i}
-              css={piece({
-                margin: `${r.nextNumber(0, 100)}%`,
-                duration: `${r.nextNumber(duration.min, duration.max) * 0.1}s`,
-                delay: `-${r.nextNumber(0, delay) * 0.1}s`,
-              })}
+              css={[
+                piece({
+                  margin: `${r.nextNumber(0, 100)}%`,
+                  duration: `${
+                    r.nextNumber(duration.min, duration.max) * 0.1
+                  }s`,
+                  delay: `-${r.nextNumber(0, delay) * 0.1}s`,
+                  keyframe: selectedKeyframe,
+                }),
+              ]}
             >
               <Piece color="#ff0000" width="10px" height="10px" />
             </div>
