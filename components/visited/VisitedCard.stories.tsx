db11c38aef6ab1072b253701100995911dcd8142
Hiroya-W <hyuyukun@gmail.com>
2023-03-31 00:48:06 +0900

    :sparkles: Add VisitedCard component

---
+++ components/visited/VisitedCard.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+
+import VisitedCard from "./VisitedCard";
+
+export default {
+  title: "Visited/VisitedCard",
+  component: VisitedCard,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof VisitedCard>;
+
+const Template: ComponentStory<typeof VisitedCard> = (args) => (
+  <VisitedCard {...args} />
+);
+
+export const StudioAquatan = Template.bind({});
+StudioAquatan.args = {
+  orgId: "1",
+  fullName: "Studio Aquatan",
+  logo: "/org_icons/studioaquatan.png",
+  logoFocus: false,
+};
+
---
+++ components/visited/VisitedCard.tsx
@@ -0,0 +1,115 @@
+import { css } from "@emotion/react";
+import Image from "next/image";
+import imgixLoader from "../../image-loader";
+
+type VisitedCardProps = {
+  orgId: string;
+  fullName: string;
+  logo: string;
+  logoFocus?: boolean;
+};
+
+const container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  max-width: 600px;
+  min-height: 70svh;
+  padding: 3.2rem;
+  margin: 2rem;
+  background-color: #fff;
+  border-radius: 0.8rem;
+  box-shadow: 0 2px 4px rgb(0 0 0 / 10%);
+
+  h1 {
+    margin: 0;
+    font-size: 3.8rem;
+    font-weight: bold;
+    text-align: center;
+    word-break: keep-all;
+    overflow-wrap: anywhere;
+  }
+`;
+
+const orgInfo = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const orgName = css`
+  padding: 0;
+  margin: 0;
+  margin-top: 1.8rem;
+  font-size: 3.2rem;
+`;
+
+const logoContainer = (logoFocus: boolean) => css`
+  position: relative;
+  width: 60%;
+  padding-bottom: 60%;
+  border: 1px solid #e5e5e5;
+  border-radius: 10px;
+
+  img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: ${logoFocus ? "contain" : "cover"};
+    border-radius: 10px;
+  }
+`;
+
+const description = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    padding: 0;
+    margin: 0;
+    font-family: GenShinGothic-P, sans-serif;
+    font-size: 2rem;
+    text-align: center;
+    word-break: keep-all;
+    overflow-wrap: anywhere;
+  }
+`;
+
+export default function VisitedCard({
+  fullName,
+  logo,
+  logoFocus = false,
+}: VisitedCardProps) {
+  return (
+    <div css={container}>
+      <h1>
+        スタンプ
+        <wbr />
+        獲得！
+      </h1>
+      <div css={orgInfo}>
+        <div css={logoContainer(logoFocus)}>
+          <Image
+            src={logo}
+            alt={fullName}
+            width={300}
+            height={300}
+            loader={imgixLoader}
+          />
+        </div>
+        <p css={orgName}>{fullName}</p>
+      </div>
+      <div css={description}>
+        <p>
+          の説明会に
+          <wbr />
+          参加しました
+        </p>
+        <p>9個中3個目のスタンプを獲得！</p>
+      </div>
+    </div>
+  );
+}
